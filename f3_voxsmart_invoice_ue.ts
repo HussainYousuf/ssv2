d8437d7a601f5b998449122f40a230dd7ccf6d4a
Hussain Yousuf <hussainyousuf@folio3.com>
2021-09-07 16:45:44 +0500

    vox work

--- f3_voxsmart_invoice_ue.ts
+++ f3_voxsmart_invoice_ue.ts
@@ -13,140 +13,157 @@ import https from "N/https";
 import http from "N/http";
 
 export function afterSubmit(context: EntryPoints.UserEvent.afterSubmitContext) {
-    if (![context.UserEventType.CREATE, context.UserEventType.EDIT, context.UserEventType.XEDIT].includes(context.type)) return;
+    try {
+        if (![context.UserEventType.CREATE, context.UserEventType.EDIT].includes(context.type)) return;
 
-    const storeId = runtime.getCurrentScript().getParameter("custscript_f3_vs_invoice_ue_store_id") || 1;
-    const esConfig = record.load({
-        type: "customrecord_external_system_config",
-        id: storeId,
-    });
-    const apikey = esConfig.getValue("custrecord_esc_password");
-    const esInfo = JSON.parse(esConfig.getValue("custrecord_esc_entity_sync_info") as string);
-    const newRecord = context.newRecord;
-    const total = newRecord.getValue("total");
-    const opportunityId = newRecord.getValue("opportunity");
+        const storeId = runtime.getCurrentScript().getParameter("custscript_f3_vs_invoice_ue_store_id") || 1;
+        const esConfig = record.load({
+            type: "customrecord_external_system_config",
+            id: storeId,
+        });
+        const apikey = esConfig.getValue("custrecord_esc_password");
+        const esInfo = JSON.parse(esConfig.getValue("custrecord_esc_entity_sync_info") as string);
+        const newRecord = context.newRecord;
+        const total = newRecord.getValue("total");
+        const opportunityId = newRecord.getValue("opportunity");
 
-    if (!opportunityId) {
-        log.debug(`${newRecord.type} id: ${newRecord.id}`, `No opportunity associated with this ${newRecord.type}`);
-        return;
-    }
+        if (!opportunityId) {
+            log.debug(`${newRecord.type} id: ${newRecord.id}`, `No opportunity associated with this ${newRecord.type}`);
+            return;
+        }
 
-    const dealId = search.create({
-        type: "customrecord_f3_es_record_data",
-        filters: [
-            ["custrecord_f3_esrd_external_system", search.Operator.EQUALTO, Number(storeId)],
-            "AND",
-            ["custrecord_f3_esrd_ns_recordtype", search.Operator.IS, search.Type.OPPORTUNITY],
-            "AND",
-            ["custrecord_f3_esrd_ns_recordid", search.Operator.IS, opportunityId]
-        ],
-        columns: ["custrecord_f3_esrd_es_recordid"]
-    }).run().getRange(0, 1)[0]?.getValue("custrecord_f3_esrd_es_recordid");
+        const searchResult = search.create({
+            type: "customrecord_f3_es_record_data",
+            filters: [
+                ["custrecord_f3_esrd_external_system", search.Operator.EQUALTO, Number(storeId)],
+                "AND",
+                ["custrecord_f3_esrd_ns_recordtype", search.Operator.IS, search.Type.OPPORTUNITY],
+                "AND",
+                ["custrecord_f3_esrd_ns_recordid", search.Operator.IS, opportunityId]
+            ],
+            columns: ["custrecord_f3_esrd_es_recordid", "custrecord_f3_esrd_ns_dep_recordid"]
+        }).run().getRange(0, 1)[0];
 
-    if (!dealId) {
-        log.debug(`Opportunity id: ${opportunityId}`, "No deal id associated with this opportunity");
-        return;
-    }
+        let esrdId = searchResult?.id;
+        const dealId = searchResult?.getValue("custrecord_f3_esrd_es_recordid");
+        const invoiceId = searchResult?.getValue("custrecord_f3_esrd_ns_dep_recordid");
 
-    const ENGAGEMENT_URL = `https://api.hubapi.com/engagements/v1/engagements?hapikey=${apikey}`;
-    const DEAL_URL = `https://api.hubapi.com/crm/v3/objects/deals/${dealId}?hapikey=${apikey}`;
-    const FILE_URL = `https://api.hubapi.com/files/v3/files?hapikey=${apikey}`;
-    const PROXY_URL = esInfo.proxy_url;
-    const MIDDLEWARE_URL = esInfo.middleware_url;
+        if (!dealId) {
+            log.debug(`Opportunity id: ${opportunityId}`, "No deal id associated with this opportunity");
+            return;
+        }
 
-    const renderFile = render.transaction({
-        entityId: newRecord.id,
-        printMode: render.PrintMode.PDF
-    });
-    const contents = renderFile.getContents();
-    const filename = renderFile.name;
+        if (Number(invoiceId) == newRecord.id) {
+            log.debug(`Invoice id: ${invoiceId}`, "Invoice already synced");
+            return;
+        }
 
-    let response = https.post({
-        url: MIDDLEWARE_URL,
-        body: JSON.stringify({
-            "files": [
-                {
-                    "key": "file",
-                    "value": contents,
-                    "options": {
-                        filename
+        const ENGAGEMENT_URL = `https://api.hubapi.com/engagements/v1/engagements?hapikey=${apikey}`;
+        const DEAL_URL = `https://api.hubapi.com/crm/v3/objects/deals/${dealId}?hapikey=${apikey}`;
+        const FILE_URL = `https://api.hubapi.com/files/v3/files?hapikey=${apikey}`;
+        const PROXY_URL = esInfo.proxy_url;
+        const MIDDLEWARE_URL = esInfo.middleware_url;
+
+        const renderFile = render.transaction({
+            entityId: newRecord.id,
+            printMode: render.PrintMode.PDF
+        });
+        const contents = renderFile.getContents();
+        const filename = renderFile.name;
+
+        let response = https.post({
+            url: MIDDLEWARE_URL,
+            body: JSON.stringify({
+                "files": [
+                    {
+                        "key": "file",
+                        "value": contents,
+                        "options": {
+                            filename
+                        }
+                    }
+                ],
+                "url": FILE_URL,
+                "folderPath": "docs",
+                "options": {
+                    "access": "PRIVATE"
+                }
+            }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).body;
+
+        log.debug("attachment response", response);
+        const { success, response: attachmentResponse } = JSON.parse(response);
+        if (!success) throw Error(`failed ${JSON.stringify(response)}`);
+
+        const { id: attachmentId, name } = (attachmentResponse as any);
+        if (!attachmentId) throw Error(response);
+
+        log.debug("pdf uploaded", name);
+
+        response = https.post({
+            url: ENGAGEMENT_URL,
+            body: JSON.stringify({
+                engagement: {
+                    type: "NOTE",
+                    active: true
+                },
+                metadata: {
+                    body: 'netsuite invoice pdf'
+                },
+                attachments: [
+                    {
+                        id: attachmentId
+                    }
+                ],
+                associations: {
+                    contactIds: [],
+                    companyIds: [],
+                    dealIds: [dealId],
+                    ownerIds: [],
+                    ticketIds: []
+                },
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*'
+            }
+        }).body;
+
+        log.debug("engagement response", response);
+        if (!JSON.parse(response).engagement) throw Error(response);
+
+        response = http.post({
+            url: PROXY_URL,
+            body: JSON.stringify({
+                url: DEAL_URL,
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    properties: {
+                        description: total
                     }
                 }
-            ],
-            "url": FILE_URL,
-            "folderPath": "docs",
-            "options": {
-                "access": "PRIVATE"
-            }
-        }),
-        headers: {
-            'content-type': 'application/json'
-        }
-    }).body;
-
-    log.debug("attachment response", response);
-    let success;
-    ({ success, response } = JSON.parse(response));
-    if (!success) throw Error(`failed ${JSON.stringify(response)}`);
-
-    const { id: attachmentId, name } = (response as any);
-    if (!attachmentId) return;
-
-    log.debug("pdf uploaded", name);
-
-    response = https.post({
-        url: ENGAGEMENT_URL,
-        body: JSON.stringify({
-            engagement: {
-                type: "NOTE",
-                active: true
-            },
-            metadata: {
-                body: 'netsuite invoice pdf'
-            },
-            attachments: [
-                {
-                    id: attachmentId
-                }
-            ],
-            associations: {
-                contactIds: [],
-                companyIds: [],
-                dealIds: [dealId],
-                ownerIds: [],
-                ticketIds: []
-            },
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': '*/*'
-        }
-    }).body;
-
-    log.debug("engagement response", response);
-
-    response = http.post({
-        url: PROXY_URL,
-        body: JSON.stringify({
-            url: DEAL_URL,
-            method: "PATCH",
+            }),
             headers: {
                 'Content-Type': 'application/json'
-            },
-            body: {
-                properties: {
-                    description: total
-                }
             }
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).body;
+        }).body;
 
-    log.debug("deal response", response);
+        log.debug("deal response", response);
 
+        if (JSON.parse(response).id) record.submitFields({
+            id: esrdId,
+            type: "customrecord_f3_es_record_data",
+            values: { "custrecord_f3_esrd_ns_dep_recordid": newRecord.id }
+        });
+        else throw Error(response);
 
-
-    // const { invoice: { userevent: { engagement_endpoint, } } } = JSON.parse(esConfig.getValue("custrecord_esc_entity_sync_info") as string);
+    } catch (error) {
+        log.error("invoice ue", JSON.stringify(error, Object.getOwnPropertyNames(error)));
+    }
 };
--- h3_base_mr.ts
+++ h3_base_mr.ts
@@ -2,7 +2,7 @@
 //  *@NApiVersion 2.1
 //  *@NScriptType MapReduceScript
 //  */
-// import { EntryPoints } from 'N/types';
+import { EntryPoints } from 'N/types';
 // import search from 'N/search';
 // import constants from './h3_constants';
 // import { getWrapper, getRecordType, areOtherDeploymentsRunning, searchRecords, scheduleScript, getOperation } from './h3_common';
@@ -54,30 +54,32 @@
 //     (getWrapper().reduce || getRecordType().reduce || getOperation().reduce)?.(context);
 // }
 
-// export function summarize(context: EntryPoints.MapReduce.summarizeContext) {
-//     (getWrapper().summarize || getRecordType().summarize || getOperation().summarize)(context);
-// }
+export function summarize(context: EntryPoints.MapReduce.reduceContext) {
+    // context.values
+    // context.output.iterator
+    // (getWrapper().summarize || getRecordType().summarize || getOperation().summarize)(context);
+}
 
 
 
-// function init() {
-//     const { EXTERNAL_STORES_CONFIG } = constants.RECORDS;
+function init() {
+    const { EXTERNAL_STORES_CONFIG } = constants.RECORDS;
 
-//     const storePermissions: { store: string, permission: string, deploymentId: string; }[] = [];
+    const storePermissions: { store: string, permission: string, deploymentId: string; }[] = [];
 
-//     searchRecords(
-//         (function (result: search.Result) {
-//             const store = result.getValue(EXTERNAL_STORES_CONFIG.FIELDS.STORE) as string;
-//             const { permissions, deploymentId } = decrypt(result.getValue(EXTERNAL_STORES_CONFIG.FIELDS.VALUE) as string);
-//             permissions.forEach(permission => storePermissions.push({ store, permission, deploymentId }));
-//         }),
-//         EXTERNAL_STORES_CONFIG.ID,
-//         [EXTERNAL_STORES_CONFIG.FIELDS.KEY, search.Operator.IS, EXTERNAL_STORES_CONFIG.KEYS.KEY],
-//         [EXTERNAL_STORES_CONFIG.FIELDS.STORE, EXTERNAL_STORES_CONFIG.FIELDS.VALUE]
-//     );
+    searchRecords(
+        (function (result: search.Result) {
+            const store = result.getValue(EXTERNAL_STORES_CONFIG.FIELDS.STORE) as string;
+            const { permissions, deploymentId } = decrypt(result.getValue(EXTERNAL_STORES_CONFIG.FIELDS.VALUE) as string);
+            permissions.forEach(permission => storePermissions.push({ store, permission, deploymentId }));
+        }),
+        EXTERNAL_STORES_CONFIG.ID,
+        [EXTERNAL_STORES_CONFIG.FIELDS.KEY, search.Operator.IS, EXTERNAL_STORES_CONFIG.KEYS.KEY],
+        [EXTERNAL_STORES_CONFIG.FIELDS.STORE, EXTERNAL_STORES_CONFIG.FIELDS.VALUE]
+    );
 
-//     scheduleScript(storePermissions);
-// }
+    scheduleScript(storePermissions);
+}
 
 // function decrypt(text: string) {
 //     return JSON.parse(text) as { permissions: string[], deploymentId: string; };
